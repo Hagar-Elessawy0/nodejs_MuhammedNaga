41841374738c82bc7c07a404e724a1b483c9d6a8
HagarElessawy0 <hagarelessawy0@gmail.com>
2024-08-27 23:00:53 +0300

    Modify CallBackFunction.js

--- src/1-TypeScript_Crash_Course/TypeKeyWord.ts
+++ src/1-TypeScript_Crash_Course/TypeKeyWord.ts
@@ -1,12 +1,18 @@
 /*
-when you use type keyword
-- Alias 
-- Union |
+How you use type keyword
+* Alias : It is a way to define a new type that can be used instead of writing the type/s directly everywhere.
+* Literal Types : It is a feature that allows you to specify that a given value can be one of a set of different types.
+* Union : It is used to define a type that can be one of several different types. It's represented by the | (pipe) symbol.
 */
 
 
-// Alias
+//? Alias
 type PI = number;
+let pi : PI = 3.14;
 
-// Union
-type status = "online" | "offline";      // either online or offline no anything else
+//? Literal Types
+type status = "online" | "offline";      //! either online or offline no anything else
+let theStatus : status;
+theStatus = "online";
+theStatus = "offline";
+//theStatus = "sleeping";   //Error
